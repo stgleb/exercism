54ce70276440be38d58164ac21264b336764f033
stgleb <glebstepanov1992@gmail.com>
2016-09-25 14:43:19 +0300

    gofmt for leap problem

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -9,8 +9,8 @@ const testVersion = 2
 // It's good style to write a comment here documenting IsLeapYear.
 func IsLeapYear(year int) bool {
 	// Write some code here to pass the test suite.
-	if year % 4 == 0 {
-		if (year % 100 == 0) && (year % 400 != 0) {
+	if year%4 == 0 {
+		if (year%100 == 0) && (year%400 != 0) {
 			return false
 		}
 
